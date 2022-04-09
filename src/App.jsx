902da7a47cedc2e37e11796ca165460ac8523909
PrettyStreett <pretty.street.003@gmail.com>
2022-04-09 14:11:45 +0600

    Added redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
-import React from 'react'
-
-import './App.css'
-import image from './assets/wave.jpg'
+import React from "react";
+import "./App.css";
+import image from "./assets/wave.jpg";
 
 const App = () => {
   return (
@@ -9,7 +8,7 @@ const App = () => {
       <h1>React Template</h1>
       <img src={image} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
