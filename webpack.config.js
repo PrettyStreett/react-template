72f785336bc9d1f26a252e939d2632834e0bdd0d
PrettyStreett <pretty.street.003@gmail.com>
2022-04-09 10:30:27 +0600

    Added Eslint and Prettier

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,37 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const config = {
-    mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+  mode: process.env.NODE_ENV === "production" ? "production" : "development",
 
-    resolve: {
-        extensions: ['.js'],
+  resolve: {
+    extensions: [".js"],
+  },
+
+  context: path.resolve(__dirname, "src"),
+
+  entry: "./index.js",
+
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "../public/index.html",
+      inject: "body",
+      hash: true,
+    }),
+  ],
+
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "public"),
     },
+    port: 8888,
+    hot: true,
+  },
+};
 
-    context: path.resolve(__dirname, "src"),
-    
-    entry: "./index.js",
-
-    output: {
-        filename: "bundle.js",
-        path: path.resolve(__dirname, "dist")
-    },
-
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: '../public/index.html',
-            inject: 'body',
-            hash: true
-        })
-    ],
-
-    devServer: { 
-        static: {
-            directory: path.join(__dirname, 'public'),
-        },
-        port: 8888,
-        hot: true
-    },   
-
-}
-
-module.exports = config
+module.exports = config;
