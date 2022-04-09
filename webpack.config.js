5c5a71577e2415498f80b3605ec3cbae4ee51311
PrettyStreett <pretty.street.003@gmail.com>
2022-04-09 12:19:20 +0600

    Added js and css loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,44 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 const config = {
   mode: process.env.NODE_ENV === "production" ? "production" : "development",
 
   resolve: {
-    extensions: [".js"],
+    extensions: [".js", ".jsx"],
   },
 
   context: path.resolve(__dirname, "src"),
 
-  entry: "./index.js",
+  entry: "./index.jsx",
 
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
 
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+
   plugins: [
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: "../public/index.html",
       inject: "body",
-      hash: true,
+      hash: false,
     }),
   ],
-
   devServer: {
     static: {
       directory: path.join(__dirname, "public"),
